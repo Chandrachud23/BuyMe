d6a698874d21b1e0718b433ee13f96b4039f610d
Chandrachud <chandrachudpati@gmail.com>
2022-02-16 06:46:02 +0530

    Homepage into smaller components and fixed styling

---
+++ src/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../components/directory/directory.component';
+import './homepage.component.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>BuyMe</h1>
+    <Directory />
+    
+  </div>
+);
+
+export default HomePage;
