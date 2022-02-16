55197a054696f0d0caf49146d2a96dcb6212ab26
Chandrachud <chandrachudpati@gmail.com>
2022-02-16 22:58:33 +0530

    Added shop page and header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
-import HomePage from './homepage/homepage.component.jsx';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header.component';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
